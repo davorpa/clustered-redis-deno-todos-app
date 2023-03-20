c06f395b8dd0e35b0c72267a2f369de11aff7a62
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-03-20 18:22:29 +0100

    lint: add typescript linter with eslint & standard

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json']
+  },
+  plugins: ['react'],
+  rules: {
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
